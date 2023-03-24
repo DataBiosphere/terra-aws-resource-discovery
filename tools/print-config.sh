9216afc95017b536da0943e248de78758ec63ad9
Dex Amundsen <dexamundsen@verily.com>
2023-03-23 21:00:52 -0400

    line

--- tools/print-config.sh
+++ tools/print-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+
 # Generate and print discovery config.json content for a given schema file and JSON payload file
 if [[ "$#" -ne 2 ]]; then
   echo "Usage: $0 <path_to_avro_schema_file> <path_to_json_payload_file>" >&2
@@ -21,13 +21,15 @@ BASE64_EXE="$(which base64)"
 # Handle differences in OSX and GNU base64 implementations
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
-  BASE64_ENCODE_COMMAND="$BASE64_EXE --input"
+  BASE64_ENCODE_COMMAND="$BASE64_EXE -i"
 else
   # GNU/Linux
   BASE64_ENCODE_COMMAND="$BASE64_EXE --wrap=0"
 fi
 
+
+
 jq -n -M \
-  --arg schema "$($BASE64_ENCODE_COMMAND "$1")" \
+  --arg schemschemaa "$($BASE64_ENCODE_COMMAND "$1")" \
   --arg payload "$($BASE64_ENCODE_COMMAND "$2")" \
   '{schema: $schema, payload: $payload}'
